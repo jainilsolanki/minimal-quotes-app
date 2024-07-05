39553874f357839433cd863181ab76b5438472c0
jainilsolanki <jainilsolanki0704@gmail.com>
2024-07-05 16:58:10 +0530

    added categories and updated endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import {
   Box,
-  FormControlLabel,
+  Chip,
   Grid,
   IconButton,
   Stack,
-  Switch,
   Tooltip,
   Typography,
-  styled,
 } from "@mui/material";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import toast, { Toaster } from "react-hot-toast";
@@ -23,73 +21,27 @@ const CopyToClipboard = async (text) => {
     console.error("Failed to copy text: ", err);
   }
 };
-const IOSSwitch = styled((props) => (
-  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-))(({ theme }) => ({
-  width: 42,
-  height: 26,
-  padding: 0,
-  "& .MuiSwitch-switchBase": {
-    padding: 0,
-    margin: 2,
-    transitionDuration: "300ms",
-    "&.Mui-checked": {
-      transform: "translateX(16px)",
-      color: "#fff",
-      "& + .MuiSwitch-track": {
-        backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "#65C466",
-        opacity: 1,
-        border: 0,
-      },
-      "&.Mui-disabled + .MuiSwitch-track": {
-        opacity: 0.5,
-      },
-    },
-    "&.Mui-focusVisible .MuiSwitch-thumb": {
-      color: "#33cf4d",
-      border: "6px solid #fff",
-    },
-    "&.Mui-disabled .MuiSwitch-thumb": {
-      color:
-        theme.palette.mode === "light"
-          ? theme.palette.grey[100]
-          : theme.palette.grey[600],
-    },
-    "&.Mui-disabled + .MuiSwitch-track": {
-      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
-    },
-  },
-  "& .MuiSwitch-thumb": {
-    boxSizing: "border-box",
-    width: 22,
-    height: 22,
-  },
-  "& .MuiSwitch-track": {
-    borderRadius: 26 / 2,
-    backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
-    opacity: 1,
-    transition: theme.transitions.create(["background-color"], {
-      duration: 500,
-    }),
-  },
-}));
+
 function App() {
   const [theme, setTheme] = useState("light");
   const [quote, setQuote] = useState({});
+  const [tag, setTag] = useState("");
   const getQuote = async () => {
     try {
-      const response = await fetch("https://api.quotable.io/random");
+      const response = await fetch(
+        `https://api.quotable.io/quotes/random?tags=${tag}`
+      );
       const quote = await response.json();
-      setQuote(quote);
-      console.log(quote);
+      setQuote(...quote);
     } catch (err) {
-      getQuote();
+      setTimeout(() => {
+        getQuote();
+      }, 250);
       toast.error("Error fetching quote from server! Retrying...");
     }
   };
 
   useEffect(() => {
-    getQuote();
     const toggleCheckbox = document.getElementById("toggle");
     const toggleHandler = function (e) {
       e.preventDefault();
@@ -108,6 +60,10 @@ function App() {
       toggleCheckbox.removeEventListener("change", toggleHandler);
     };
   }, []);
+
+  useEffect(() => {
+    getQuote();
+  }, [tag]);
 
   return (
     <>
@@ -313,6 +269,58 @@ function App() {
                   </IconButton>
                 </Box>
               </Tooltip>
+            </Stack>
+            <Stack
+              direction={"row"}
+              gap={1}
+              alignItems={"center"}
+              flexWrap={"wrap"}
+            >
+              <Typography
+                sx={{
+                  color: theme === "light" ? "#000" : "#fff",
+                  fontFamily: "IBM Plex Mono , monospace",
+                  transition: "all 0.4s ease-in-out",
+                }}
+              >
+                Get by tags
+              </Typography>
+
+              <Chip
+                label="Any"
+                onClick={() => setTag("")}
+                variant={tag === "" ? "outlined" : "filled"}
+              />
+              <Chip
+                label="Love"
+                onClick={() => setTag("love")}
+                variant={tag === "love" ? "outlined" : "filled"}
+              />
+              <Chip
+                label="Happiness"
+                onClick={() => setTag("happiness")}
+                variant={tag === "happiness" ? "outlined" : "filled"}
+              />
+              <Chip
+                label="Technology"
+                onClick={() => setTag("technology")}
+                variant={tag === "technology" ? "outlined" : "filled"}
+              />
+              <Chip
+                label="Famous Quotes"
+                onClick={() => setTag("famous-quotes")}
+                variant={tag === "famous-quotes" ? "outlined" : "filled"}
+              />
+              <Chip
+                label="Sports"
+                onClick={() => setTag("sports")}
+                variant={tag === "sports" ? "outlined" : "filled"}
+              />
+              <Chip
+                label="Wisdom"
+                onClick={() => setTag("wisdom")}
+                variant={tag === "wisdom" ? "outlined" : "filled"}
+              />
             </Stack>
           </Stack>
         </Grid>
